f2be36774eac269a11182635b4fc6c5dee2f1a00
Ahmed Salem <a.salem@arabhosters.com>
2018-07-02 18:55:52 +0200

    enhance code to work with each case of two numbers after atom

--- index.js
+++ index.js
@@ -86,6 +86,9 @@ for(let i=0; i<text.length; i++){
       }
     } else if(!_.isNaN(Number(nextChar))){
       value = Number(nextChar);
+      if(afterNextChar && !_.contains(['[', ']'], afterNextChar) && !_.isNaN(Number(afterNextChar))){
+        value = Number(nextChar + afterNextChar);
+      }
     }
 
     _.each(finalStartEnd, (multi) => {
